172167ec0f9a3298acfb335f765a7737fdd21df3
Josiah Niedrauer <jniedrauer@gmail.com>
2019-11-19 12:50:38 -0800

    Use standard blockchyp config file for tests

--- src/test/java/com/blockchyp/client/itest/IntegrationTestSettings.java
+++ src/test/java/com/blockchyp/client/itest/IntegrationTestSettings.java
@@ -1,55 +1,75 @@
 package com.blockchyp.client.itest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
 public class IntegrationTestSettings {
 
-    private String gatewayHost;
-    private String testGatewayHost;
+    private String gatewayUrl;
+    private String testGatewayUrl;
     private String defaultTerminalName;
     private String defaultTerminalAddress;
     private String apiKey;
     private String bearerToken;
     private String signingKey;
-    public String getGatewayHost() {
-        return gatewayHost;
+
+    @JsonProperty("gateway")
+    public String getGatewayUrl() {
+        return gatewayUrl;
     }
-    public void setGatewayHost(String gatewayHost) {
-        this.gatewayHost = gatewayHost;
+
+    public void setGatewayUrl(String gatewayUrl) {
+        this.gatewayUrl = gatewayUrl;
     }
+
     public String getDefaultTerminalName() {
         return defaultTerminalName;
     }
+
     public void setDefaultTerminalName(String defaultTerminalName) {
         this.defaultTerminalName = defaultTerminalName;
     }
+
     public String getDefaultTerminalAddress() {
         return defaultTerminalAddress;
     }
+
     public void setDefaultTerminalAddress(String defaultTerminalAddress) {
         this.defaultTerminalAddress = defaultTerminalAddress;
     }
+
     public String getApiKey() {
         return apiKey;
     }
+
     public void setApiKey(String apiKey) {
         this.apiKey = apiKey;
     }
+
     public String getBearerToken() {
         return bearerToken;
     }
+
     public void setBearerToken(String bearerToken) {
         this.bearerToken = bearerToken;
     }
+
     public String getSigningKey() {
         return signingKey;
     }
+
     public void setSigningKey(String signingKey) {
         this.signingKey = signingKey;
     }
-    public String getTestGatewayHost() {
-        return testGatewayHost;
+
+    @JsonProperty("testGateway")
+    public String getTestGatewayUrl() {
+        return testGatewayUrl;
     }
-    public void setTestGatewayHost(String testGatewayHost) {
-        this.testGatewayHost = testGatewayHost;
+
+    public void setTestGatewayUrl(String testGatewayUrl) {
+        this.testGatewayUrl = testGatewayUrl;
     }
 
 
