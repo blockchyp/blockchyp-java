d808bbe5f8e8c10515aa080a78d9e16b3eda8b0c
Josiah Niedrauer <jniedrauer@gmail.com>
2019-11-19 14:42:34 -0800

    Add tests for EBT cards

---
+++ src/test/java/com/blockchyp/client/itest/EbtManualChargeTest.java
@@ -0,0 +1,38 @@
+package com.blockchyp.client.itest;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.blockchyp.client.BlockChypClient;
+import com.blockchyp.client.dto.AuthorizationRequest;
+import com.blockchyp.client.dto.AuthorizationResponse;
+import com.blockchyp.client.dto.CardType;
+
+public class EbtManualChargeTest {
+
+    @Test
+    @Category(IntegrationTest.class)
+    public void testTransaction() throws Exception {
+
+
+        BlockChypClient client = IntegrationTestConfiguration.getTestClient();
+
+        AuthorizationRequest request = new AuthorizationRequest();
+        request.setManualEntry(true);
+        request.setAmount("55.55");
+        request.setTest(true);
+        request.setTerminalName(IntegrationTestConfiguration.getDefaultTerminalName());
+        request.setCardType(CardType.EBT);
+
+
+        AuthorizationResponse response = client.charge(request);
+
+        Assert.assertNotNull(response);
+        Assert.assertTrue(response.isApproved());
+
+    }
+
+}
