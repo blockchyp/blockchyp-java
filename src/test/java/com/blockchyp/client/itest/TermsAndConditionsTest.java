6d0e4f65c6df2f58edf8f18070d0374f7283407e
BlockChyp SDK Builder <devops@blockchyp.com>
2020-01-02 22:33:50 +0000

    Merge pull request #27 from blockchyp/feature/CHYP-1044
    Feature/chyp 1044

--- src/test/java/com/blockchyp/client/itest/TermsAndConditionsTest.java
+++ src/test/java/com/blockchyp/client/itest/TermsAndConditionsTest.java
@@ -38,7 +38,7 @@ public class TermsAndConditionsTest extends BaseTestCase {
          request.setTest(true);
          request.setTerminalName("Test Terminal");
          request.setTcName("HIPPA Disclosure");
-         request.setTcContent("Full contract text");
+         request.setTcContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ullamcorper id urna quis pulvinar. Pellentesque vestibulum justo ac nulla consectetur tristique. Suspendisse arcu arcu, viverra vel luctus non, dapibus vitae augue. Aenean ac volutpat purus. Curabitur in lacus nisi. Nam vel sagittis eros. Curabitur faucibus ut nisl in pulvinar. Nunc egestas, orci ut porttitor tempus, ante mauris pellentesque ex, nec feugiat purus arcu ac metus. Cras sodales ornare lobortis. Aenean lacinia ultricies purus quis pharetra. Cras vestibulum nulla et magna eleifend eleifend. Nunc nibh dolor, malesuada ut suscipit vitae, bibendum quis dolor. Phasellus ultricies ex vitae dolor malesuada, vel dignissim neque accumsan.");
          request.setSigFormat(SignatureFormat.PNG);
          request.setSigWidth(200);
          request.setSigRequired(true);
