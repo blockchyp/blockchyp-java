3603b00fe69f52dfbb4c885266448978a956dbfd
BlockChyp SDK Builder <devops@blockchyp.com>
2020-09-29 00:04:59 +0000

    added a partial refund test

---
+++ src/test/java/com/blockchyp/client/itest/PartialRefundTest.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2019 BlockChyp, Inc. All rights reserved. Use of this code is governed by a
+ * license that can be found in the LICENSE file.
+ *
+ * This file was generated automatically. Changes to this file will be lost every time the
+ * code is regenerated.
+ */
+
+package com.blockchyp.client.itest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.blockchyp.client.BlockChypClient;
+import com.blockchyp.client.IntegrationTest;
+import com.blockchyp.client.IntegrationTestConfiguration;
+import com.blockchyp.client.dto.RefundRequest;
+import com.blockchyp.client.dto.AuthorizationResponse;
+import com.blockchyp.client.dto.AuthorizationRequest;
+
+public class PartialRefundTest extends BaseTestCase {
+
+    @Test
+    @Category(IntegrationTest.class)
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void testTransaction() throws Exception {
+
+        BlockChypClient client = IntegrationTestConfiguration.getTestClient();
+
+        processTestDelay(client, "PartialRefundTest");
+
+        // Set request parameters
+        AuthorizationRequest setupRequest = new AuthorizationRequest();
+        setupRequest.setPan("4111111111111111");
+        setupRequest.setAmount("25.55");
+        setupRequest.setTest(true);
+        setupRequest.setTransactionRef(getUUID());
+
+         AuthorizationResponse setupResponse = client.charge(setupRequest);
+
+        // Set request parameters
+        RefundRequest request = new RefundRequest();
+        request.setTransactionId(setupResponse.getTransactionId());
+        request.setAmount("5.00");
+        request.setTest(true);
+
+        AuthorizationResponse response = client.refund(request);
+
+        // Response assertions
+        Assert.assertTrue(response.isSuccess());
+        Assert.assertTrue(response.isApproved());
+
+    }
+
+}
