fa4bac3ffad2658a1321e140e3c81f84113a37da
BlockChyp SDK Builder <devops@blockchyp.com>
2023-11-03 19:57:21 +0000

    added shipmentNumber and shipmentCount

--- src/main/java/com/blockchyp/client/dto/CaptureRequest.java
+++ src/main/java/com/blockchyp/client/dto/CaptureRequest.java
@@ -54,6 +54,10 @@ public class CaptureRequest implements ITimeoutRequest, ICoreRequest, IPreviousT
 
      private String taxAmount;
 
+     private int shipmentCount;
+
+     private int shipmentNumber;
+
     /**
      * Sets the request timeout in seconds.
      * @param value the request timeout in seconds.
@@ -403,6 +407,41 @@ public class CaptureRequest implements ITimeoutRequest, ICoreRequest, IPreviousT
      @JsonProperty("taxAmount")
      public String getTaxAmount() {
           return this.taxAmount;
+     }
+
+    /**
+     * Sets the number of shipments the original authorization will be broken into.
+     * @param value the number of shipments the original authorization will be broken
+     * into.
+     */
+     public void setShipmentCount(int value) {
+          this.shipmentCount = value;
+     }
+
+    /**
+     * Gets the number of shipments the original authorization will be broken into.
+     * @return the number of shipments the original authorization will be broken into.
+     */
+     @JsonProperty("shipmentCount")
+     public int getShipmentCount() {
+          return this.shipmentCount;
+     }
+
+    /**
+     * Sets which shipment this particular capture is for.
+     * @param value which shipment this particular capture is for.
+     */
+     public void setShipmentNumber(int value) {
+          this.shipmentNumber = value;
+     }
+
+    /**
+     * Gets which shipment this particular capture is for.
+     * @return which shipment this particular capture is for.
+     */
+     @JsonProperty("shipmentNumber")
+     public int getShipmentNumber() {
+          return this.shipmentNumber;
      }
 
 }
